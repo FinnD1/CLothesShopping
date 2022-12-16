cae7b33a7cdaa2becb1e087b3e3fa0b19b2d37a7
Qing-yun02 <118549859+Qing-yun02@users.noreply.github.com>
2022-12-16 21:44:06 +0700

    Update them LinkAnh trong form Quan Ao[Van]

--- Test App/Program.cs
+++ Test App/Program.cs
@@ -16,7 +16,7 @@ namespace Test_App
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new GiaoDienChinh());
+            Application.Run(new QuanAo());
         }
     }
 }
